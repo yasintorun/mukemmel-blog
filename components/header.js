ba5873f18e17b37e60dda85eea17d8d72ac3eb6b
Yasin Torun <58103649+yasintorun@users.noreply.github.com>
2019-12-20 20:34:02 +0300

    Header.js Componenti Oluşturuldu

---
+++ components/header.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import Link from 'next/Link';
+
+const Header = () => {
+    return (
+        <div className="hero">
+            <h1 className="hero-title">Selman Kahya</h1>
+            <div className="hero-social-links">
+                <Link href="https://medium.com/@selmankahya">
+                    <a className="social-link">Medium</a>
+                </Link>
+                <Link href="https://www.twitter.com/selmankahyax">
+                    <a className="social-link">Twitter</a>
+                </Link>
+                <Link href="https://www.linkedin.com/in/selmankahya">
+                    <a className="social-link">LinkedIn</a>
+                </Link>
+                <Link href="https://www.instagram.com/selmankahyax/?hl=en">
+                    <a className="social-link">Instagram</a>
+                </Link>
+            </div>
+            <style jsx>{`
+      .container {
+        max-width: 650px;
+        width: 100%;
+        margin: 0 auto;
+      }
+
+      .hero {
+        text-align: center;
+        margin: 96px 0;
+      }
+
+      .social-link {
+        margin-right: 8px;
+      }
+
+      .hero-title {
+        font-size: 48px;
+      }
+
+      .blog-date {
+        text-align: right;
+        color: #cccccc;
+        margin: 12px 0 48px 0;
+      }
+
+      a {
+        color: #35459e;
+        text-decoration: none;
+      }
+                
+            `}
+            </style>
+        </div>
+    )
+
+}
+export default Header;
