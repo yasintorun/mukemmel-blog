e2bddb41051b2c29b33e7942e4c42b491001b13a
Yasin Torun <58103649+yasintorun@users.noreply.github.com>
2019-12-20 21:52:15 +0300

    İletişim sayfası oluşturuldu

---
+++ pages/iletisim.js
@@ -0,0 +1,17 @@
+import _Head from "../components/Head";
+import Header from "../components/header";
+
+const Contact = () => (
+    <div>
+        <_Head title="Bana Ulaşmak ister misin" />
+        <h1>Bana ulaşabilecegin Yerler</h1>
+        <ul>
+            <li><a href="#">Instagram</a></li>
+            <li><a href="#">Twitter</a></li>
+            <li><a href="#">LinkedIn</a></li>
+            <li><a href="#">Facebook</a></li>
+            <li>E-mail: deneme@deneme.com</li>
+        </ul>
+    </div>
+)
+export default Contact;
