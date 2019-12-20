3ad3ec122504e6b2bbca5ac0dba21d6df6f31efc
Yasin Torun <58103649+yasintorun@users.noreply.github.com>
2019-12-20 21:46:22 +0300

    Hakkimda sayfası oluşturuldu

---
+++ pages/hakkimda.js
@@ -0,0 +1,19 @@
+import _Head from '../components/Head';
+import Header from '../components/header';
+
+const AboutMe = () => (
+    <div>
+        <_Head title="Hakkımda Ben Kimim!" />
+        <Header />
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+            Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+            Excepteur sint occaecat cupidatat non proident,
+            sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+    </div>
+)
+
+export default AboutMe;
