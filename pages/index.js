56f43efc422c3e94b2cc546b99be187a6a831e29
Yasin Torun <58103649+yasintorun@users.noreply.github.com>
2019-12-20 20:37:30 +0300

    Kod tekrarı düzeltildi. Header componenti kullanıldı

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import fetch from "isomorphic-unfetch";
 import Head from "next/head";
 import Link from "next/link";
 import ReactMarkdown from "react-markdown";
+import Header from "../components/header";
 
 const Home = ({ posts }) => (
   <div className="container">
@@ -11,23 +12,7 @@ const Home = ({ posts }) => (
       <link rel="icon" href="/favicon.ico" />
     </Head>
 
-    <div className="hero">
-      <h1 className="hero-title">Selman Kahya</h1>
-      <div className="hero-social-links">
-        <Link href="https://medium.com/@selmankahya">
-          <a className="social-link">Medium</a>
-        </Link>
-        <Link href="https://www.twitter.com/selmankahyax">
-          <a className="social-link">Twitter</a>
-        </Link>
-        <Link href="https://www.linkedin.com/in/selmankahya">
-          <a className="social-link">LinkedIn</a>
-        </Link>
-        <Link href="https://www.instagram.com/selmankahyax/?hl=en">
-          <a className="social-link">Instagram</a>
-        </Link>
-      </div>
-    </div>
+    <Header />
 
     {posts.map(post => (
       <div className="blog">
