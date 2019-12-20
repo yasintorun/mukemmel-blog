74721652e9c3cf0adeb52d131f4ff112bc1fb070
Yasin Torun <58103649+yasintorun@users.noreply.github.com>
2019-12-20 21:16:11 +0300

    _Head componenti oluşturuldu

---
+++ components/Head.js
@@ -0,0 +1,9 @@
+import Head from 'next/Head';
+
+const _Head = () => (
+    <Head>
+        <title>Home</title>
+        <link rel="icon" href="/favicon.ico" />
+    </Head>
+)
+export default _Head;
