bbe7eae1b414df9a77424f59ee2744a90cb6e2cb
Yasin Torun <58103649+yasintorun@users.noreply.github.com>
2019-12-21 12:24:12 +0300

    Google iconları kullanabilmek için head componentine eklendi.

--- components/Head.js
+++ components/Head.js
@@ -5,6 +5,8 @@ const _Head = ({ title }) => (
         <title>{title}</title>
         <link rel="icon" href="/favicon.ico" />
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous" />
+
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
     </Head>
 )
 export default _Head;
