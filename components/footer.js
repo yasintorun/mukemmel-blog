4eb4926d05f4de40e218e343d86a66b495b51b39
Yasin Torun <58103649+yasintorun@users.noreply.github.com>
2019-12-21 14:03:28 +0300

    Css eklendi görünüm iyileştirildi

--- components/footer.js
+++ components/footer.js
@@ -1,8 +1,18 @@
 import React from 'react';
 
 const Footer = () => (
-    <div>
-        <p>&copy; Copyright 2019<a href="http://www.mukemmel.blog/" target="_blank">Mukemmel.blog</a></p>
+    <div className="footer">
+        <p>&copy; Copyright 2019 <a href="http://www.mukemmel.blog/" target="_blank">Mukemmel.blog</a></p>
+
+        <style jsx>{`
+            .footer {
+                background-color: #333;
+                padding: 10px 0;
+                margin: 8px 0;
+                color: white;
+                text-align: center;
+            }
+        `}</style>
     </div>
 )
 export default Footer;
