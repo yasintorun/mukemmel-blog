a396fc8dccd3c8e879b749c54f495dfdabfe4dad
Yasin Torun <58103649+yasintorun@users.noreply.github.com>
2019-12-21 14:08:08 +0300

    Gerek kalmadıgı için css ler silindi.

--- pages/[postId].js
+++ pages/[postId].js
@@ -12,37 +12,7 @@ const BlogPost = ({ post }) => (
     <Blog post={post} />
 
     <Footer />
-    <style jsx>{`
-      .container {
-        max-width: 650px;
-        width: 100%;
-        margin: 0 auto;
-      }
 
-      .hero {
-        text-align: center;
-        margin: 96px 0;
-      }
-
-      .social-link {
-        margin-right: 8px;
-      }
-
-      .hero-title {
-        font-size: 48px;
-      }
-
-      .blog-date {
-        text-align: right;
-        color: #cccccc;
-        margin: 12px 0 48px 0;
-      }
-
-      a {
-        color: #35459e;
-        text-decoration: none;
-      }
-    `}</style>
   </div>
 );
 
