3d3900fd8d0e1b0879181b5b07154926a06ae09c
Yasin Torun <58103649+yasintorun@users.noreply.github.com>
2019-12-21 13:51:51 +0300

    sosyal medya Linkleri tekrar eklendi. Linklerdeki yazıları iconlarıyla degiştirildi. css eklendi

--- components/header.js
+++ components/header.js
@@ -1,47 +1,45 @@
 import React from 'react';
-import Link from 'next/Link';
 import Nav from './nav';
-
+import Link from 'next/link'
 const Header = () => (
   <div className="hero">
-    <h1 className="hero-title">Selman Kahya</h1>
-
+    <h1 className="hero-title"><a href="/">Selman Kahya</a></h1>
+    <div className="hero-social-links">
+      <Link href="https://medium.com/@selmankahya">
+        <a className="social-link"><span className="	fa fa-medium" /></a>
+      </Link>
+      <Link href="https://www.twitter.com/selmankahyax">
+        <a className="social-link"><span className="	fa fa-twitter" /></a>
+      </Link>
+      <Link href="https://www.linkedin.com/in/selmankahya">
+        <a className="social-link"><span className="	fa fa-linkedin" /></a>
+      </Link>
+      <Link href="https://www.instagram.com/selmankahyax/?hl=en">
+        <a className="social-link"><span className="	fa fa-instagram" /></a>
+      </Link>
+    </div>
+    <Nav />
     <style jsx>{`
-      .container {
-        max-width: 650px;
-        width: 100%;
-        margin: 0 auto;
-      }
-
-      .hero {
+      .hero{
         text-align: center;
-        margin: 96px 0;
+        margin: 48px 0;
       }
-
-      .social-link {
-        margin-right: 8px;
-      }
-
-      .hero-title {
+      .hero-title a{
         font-size: 48px;
+        color: #333;
       }
-
-      .blog-date {
-        text-align: right;
-        color: #cccccc;
-        margin: 12px 0 48px 0;
-      }
-
-      a {
-        color: #35459e;
+      a{
         text-decoration: none;
       }
-                
-            `}
-    </style>
-    <div>
-      <Nav />
-    </div>
+      .social-link{
+        margin-right: 10px;
+        color: tomato;
+      }
+      .social-link:hover{
+        color: red;
+      }
+      
+    `}</style>
   </div>
 
 )
