16702cb9a41e59029aaa398f0a8dfbcf5b84c769
Yasin Torun <58103649+yasintorun@users.noreply.github.com>
2019-12-26 20:32:40 +0300

    Ufak css degişiklikleri

--- pages/hakkimda.js
+++ pages/hakkimda.js
@@ -5,14 +5,24 @@ const AboutMe = () => (
     <div>
         <_Head title="Hakkımda Ben Kimim!" />
         <Header />
-        <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-            Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-            Excepteur sint occaecat cupidatat non proident,
-            sunt in culpa qui officia deserunt mollit anim id est laborum.
-        </p>
+        <div className="container">
+            <p className="text">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                Excepteur sint occaecat cupidatat non proident,
+                sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+        </div>
+        <style jsx>{`
+            .text{
+                max-width: 540px;
+                width: 100%;
+                margin: auto;
+                font-size: 20px;
+            }
+        `}</style>
     </div>
 )
 
